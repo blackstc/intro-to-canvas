1025b95b79bf0f382d01891dbb86319dd59a5e0b
Blackstock <charlieblackstock@gmail.com>
2015-08-11 17:09:46 -0600

    added first code for canvas squares

--- canvas.js
+++ canvas.js
@@ -1,6 +1,30 @@
-// function drawBorder(){
-//     var canvas = document.getElementById("canvas");
-//     var context = canvas.getContext('2d');
-//     var size = { x: canvas.width, y: canvas.height }
-//     context.strokeRect(0,0, size.x, size.y);
-// }
+$(document).ready(function() {
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+  var size = { x: canvas.width, y: canvas.height };
+  var startX = Math.floor(Math.random()*size.x);
+  var startY = Math.floor(Math.random()*size.y);
+  var endX = Math.floor(Math.random()*(size.x - startX) + startX);
+  var endY = Math.floor(Math.random()*(size.y - startY) + startY);
+
+  function drawBorder(){
+    context.strokeRect(0,0, size.x, size.y);
+  }
+
+  drawBorder();
+
+  $("#canvas").on("click", function() {
+    context.fillStyle = "blue";
+    context.fillRect(0, 0, size.x/2, size.y/2);
+  });
+
+  $("#canvas").on("dblclick", function() {
+    context.fillStyle = "red";
+    context.fillRect(size.x/2, size.y/2, size.x, size.y);
+  });
+
+  $("#red").on("click", function() {
+    context.fillStyle = "red";
+    context.fillRect(startX, startY, endX, endY);
+  });
+});
