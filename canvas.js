f27f98bdd6077916a8e116e0911509ac4058ffd9
Blackstock <charlieblackstock@gmail.com>
2015-08-11 21:38:20 -0600

    added JS for second iteration of canvas

--- canvas.js
+++ canvas.js
@@ -2,12 +2,35 @@ $(document).ready(function() {
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext('2d');
   var size = { x: canvas.width, y: canvas.height };
+  var globalAlpha = 1;
 
   function drawBorder(){
     context.strokeRect(0,0, size.x, size.y);
   }
 
-  drawBorder();
+  function drawSquare(color) {
+    var startX = Math.floor(Math.random()*size.x);
+    var startY = Math.floor(Math.random()*size.y);
+    var endX = Math.floor(Math.random()*(size.x - startX) + startX);
+    var endY = Math.floor(Math.random()*(size.y - startY) + startY);
+    context.fillStyle = color;
+    context.fillRect(startX, startY, endX, endY);
+    context.globalAlpha = globalAlpha;
+    globalAlpha -= 0.1;
+  }
+
+  function drawCircle(color) {
+    var startX = Math.floor(Math.random()*size.x);
+    var startY = Math.floor(Math.random()*size.y);
+    var radius = Math.floor(Math.random()*size.y/2);
+    context.beginPath();
+    context.arc(startX, startY, radius, 0, 2 * Math.PI);
+    context.fillStyle = color;
+    context.fill();
+    context.stroke();
+    context.globalAlpha = globalAlpha;
+    globalAlpha -= 0.1;
+  }
 
   $("#canvas").on("click", function() {
     context.fillStyle = "blue";
@@ -19,42 +42,21 @@ $(document).ready(function() {
     context.fillRect(size.x/2, size.y/2, size.x, size.y);
   });
 
-  $("#red").on("click", function() {
-    var startX = Math.floor(Math.random()*size.x);
-    var startY = Math.floor(Math.random()*size.y);
-    var endX = Math.floor(Math.random()*(size.x - startX) + startX);
-    var endY = Math.floor(Math.random()*(size.y - startY) + startY);
-    context.fillStyle = $(this).html();
-    context.fillRect(startX, startY, endX, endY);
+  $("button").on("click", function() {
+    var random = Math.round(Math.random());
+    if (random === 1) {
+      drawCircle($(this).html());
+    } else {
+      drawSquare($(this).html());
+    }
   });
 
-$("#blue").on("click", function() {
-    var startX = Math.floor(Math.random()*size.x);
-    var startY = Math.floor(Math.random()*size.y);
-    var endX = Math.floor(Math.random()*(size.x - startX) + startX);
-    var endY = Math.floor(Math.random()*(size.y - startY) + startY);
-    context.fillStyle = $(this).html();
-    context.fillRect(startX, startY, endX, endY);
+  $("#clear").on("click", function() {
+    context.clearRect(0, 0, size.x, size.y);
+    globalAlpha = 1;
+    context.globalAlpha = globalAlpha;
+    drawBorder();
   });
 
-$("#green").on("click", function() {
-    var startX = Math.floor(Math.random()*size.x);
-    var startY = Math.floor(Math.random()*size.y);
-    var endX = Math.floor(Math.random()*(size.x - startX) + startX);
-    var endY = Math.floor(Math.random()*(size.y - startY) + startY);
-    context.fillStyle = $(this).html();
-    context.fillRect(startX, startY, endX, endY);
-  });
-
-$("#orange").on("click", function() {
-    var startX = Math.floor(Math.random()*size.x);
-    var startY = Math.floor(Math.random()*size.y);
-    var endX = Math.floor(Math.random()*(size.x - startX) + startX);
-    var endY = Math.floor(Math.random()*(size.y - startY) + startY);
-    context.fillStyle = $(this).html();
-    context.fillRect(startX, startY, endX, endY);
-  });
-
-
-
+  drawBorder();
 });
